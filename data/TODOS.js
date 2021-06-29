983f52e5d828fff14c422927a14deefd5214f149
Peter <pete225848@gmail.com>
2021-06-29 12:32:47 -0700

    changed data files to work to work with todos

---
+++ data/TODOS.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    todo: 'clean bedroom',
+    completed: false,
+    user_id: 1
+  },
+  {
+    todo: 'wash the dishes',
+    completed: false,
+    user_id: 1
+  },
+  {
+    todo: 'mowe the lawn',
+    completed: false,
+    user_id: 1
+  },
+  {
+    todo: 'vacuum living room',
+    completed: false,
+    user_id: 1
+  },
+  {
+    todo: 'wash my car',
+    completed: false,
+    user_id: 1
+  }
+];
